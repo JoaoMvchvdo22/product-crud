dff0713829df7e4ede647212a6be139080881e5b
JoaoMachado22 <joao.vicente@evolutix.com.br>
2023-12-03 15:54:29 -0300

    change api routes

--- src/app/shared-service.service.ts
+++ src/app/shared-service.service.ts
@@ -9,25 +9,27 @@ export class SharedServiceService {
 
   showOptions: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
+  apiRestUrl: string = "https://server-product-production.up.railway.app/products";
+
   constructor(private http: HttpClient) { }
 
   post(body: any): Observable<any> {
-    return this.http.post('http://localhost:3333/products', body);
+    return this.http.post(this.apiRestUrl, body);
   }
 
   get(): Observable<any> {
-    return this.http.get('http://localhost:3333/products');
+    return this.http.get(this.apiRestUrl);
   }
 
   update(id: number, body: any): Observable<any> {
-    return this.http.put(`http://localhost:3333/products/${id}`, body);
+    return this.http.put(this.apiRestUrl + `/${id}`, body);
   }
 
   delete(id: number): Observable<any> {
-    return this.http.delete(`http://localhost:3333/products/${id}`);
+    return this.http.delete(this.apiRestUrl + `/${id}`);
   }
 
   getDetail(id: number): Observable<any> {
-    return this.http.get(`http://localhost:3333/products/${id}`);
+    return this.http.get(this.apiRestUrl + `/${id}`);
   }
 }
